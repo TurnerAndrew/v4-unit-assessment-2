11ee4f50bf73156bd638a776b7578d849c25afe5
Andrew Turner <turner.andrew0@gmail.com>
2020-11-15 10:19:34 -0600

    updated assesment

--- js-in-the-dom/index.js
+++ js-in-the-dom/index.js
@@ -1,14 +1,63 @@
 const formBtn = document.getElementById('close-form')
-let form = document.getElementsByClassName('form')
+const form = document.getElementsByClassName('form')[0]
 
-function toggler(){
+function toggler() {
     form.classList.toggle('hide')
-    //toggle form classList to/from #hide
-    form.innerText === 'X' ? form.innerText === '+' ? form.innerText === '+' : form.innerText === X
-    //conditional to check innerText === X
-    //x -> + and vice versa
+    formBtn.innerText === 'X' ? formBtn.innerText = '+' : formBtn.innerText = 'X'
 }
 
-formBtn.addEventListener('click', () => {
-    toggler()
+formBtn.addEventListener ('click', toggler)
+
+const nameInput = document.getElementById('name')
+const emailInput = document.getElementById('email')
+let emailList = [] //stores user input
+
+function validateForm (){
+    if (nameInput.value != true && emailInput.value != true){
+        nameInput.style = 'border: 2px solid red'
+        emailInput.style = 'border: 2px solid red'
+        window.alert('You must enter a valid name and e-mail address to subscribe')
+    } else if(nameInput.value != true){
+        nameInput.style = 'border: 2px solid red'
+        window.alert('Please enter your name to subscribe')
+    } else if(emailInput.value != true){
+        emailInput.style = 'border: 2px solid red'
+        window.alert('Please enter your e-mail address to subscribe')
+    } else {
+        emailList.push[nameInput.value, emailInput.value]
+        displayThankYou()
+    }
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    validateForm()
 })
+
+const formContainer = document.getElementsByClassName('form-container')[0]
+
+function displayThankYou(){
+    formContainer.innerText('Thank you for subscribing!')
+}
+
+nameInput.addEventListener('change', () => nameInput.style = 'border: none')
+emailInput.addEventListener('change', () => emailInput.style = 'border: none')
+
+let cart = document.createElement('div')
+cart.className = 'cart'
+let main = document.querySelector('main')
+let cartItems = 0
+
+function addToCart(){
+    if(cartItems === 0){
+        cartItems = 1
+        cart.setAttribute('class', 'cart-display')
+        cart.innerText('Your Cart: 1 item')
+        main.appendChild(cart)
+    } 
+    else {
+        cartItems += 1
+        cart.innerText = "Your Cart: " + cartItems
+    }
+    
+}
